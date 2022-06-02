7912a015f3beb4ff649f0b1a339ffb555330fb2f
Cintia Arce <cintiaayelenarce@gmail.com>
2022-06-02 14:39:24 -0300

    clean readme & components

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom' 
-import LandingPage from './components/Landing-Page';
-import Home from './components/Home';
-import RecipeDetail from './components/Recipe-Detail';
-import CreateRecipe from './components/Create-Recipe';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/' element={<LandingPage />}/>
-          <Route exact path='/home/recipes' element={<Home />}/>
-          <Route exact path='/home/recipes/:id' element={<RecipeDetail />} />
-          <Route exact path='/home/recipe' element={<CreateRecipe />} />
         </Routes> 
       </div>
     </BrowserRouter>
